4b69af53e8931130ba6b02765a45caf2776c8709
Hovo Serobyan <129414253+Hovhannes09@users.noreply.github.com>
2026-05-31 23:56:02 +0400

    Added log for migate.js about succsessfuly created tables

--- migrate.js
+++ migrate.js
@@ -3,13 +3,18 @@ import sequelize from './clients/db.mysql.js'
 import './models/appUsers.js'
 import './models/customers.js'
 import './models/directoryUsers.js'
-import './models/orders.js'
 import './models/persons.js'
+import './models/orders.js'
 
 export async function migrate() {
-	console.log('Running Migration')
+	console.log('Running migration...')
 
 	await sequelize.sync({ alter: true })
 
-	console.log('Migration finished succeessfuly')
+	console.log('-> app_users table successfully created')
+	console.log('-> customers table successfully created')
+	console.log('-> directory_users table successfully created')
+	console.log('-> persons table successfully created')
+	console.log('-> orders table successfully created')
+	console.log('Migration finished successfully.')
 }
