dd11c32a07a09f7f18a8e349fc4f9e1e70b9824c
Hovo Serobyan <129414253+Hovhannes09@users.noreply.github.com>
2026-05-31 23:43:00 +0400

    Changed appUsers.js with Sequelize

--- models/appUsers.js
+++ models/appUsers.js
@@ -1,43 +1,53 @@
-import DbMysql from "../clients/db.mysql.js";
-import _ from "lodash";
+import _ from 'lodash'
+import sequelize from '../clients/db.mysql.js'
+import { DataTypes } from 'sequelize'
+
+const AppUser = sequelize.define(
+	'AppUser',
+	{
+		id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+		name: { type: DataTypes.STRING(50), allowNull: false },
+		email: { type: DataTypes.STRING(255), allowNull: false, unique: true },
+		password: { type: DataTypes.STRING(255), allowNull: false },
+		created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+	},
+	{
+		tableName: 'app_users',
+		timestamps: false
+	}
+)
 
 export async function findByEmail(email) {
-  try {
-    const [rows] = await DbMysql.query(
-      "SELECT * FROM app_users WHERE email = ?",
-      [email],
-    );
-    return _.head(rows) ?? null;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+	try {
+		const user = await AppUser.findOne({ where: { email } })
+		return _.defaultTo(user, null)
+	} catch (error) {
+		console.error(error)
+		return null
+	}
 }
 
 export async function createUser({ name, email, password }) {
-  try {
-    const [result] = await DbMysql.query(
-      "INSERT INTO app_users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, password],
-    );
-    return result.insertId;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+	try {
+		const user = await AppUser.create({ name, email, password })
+		return user.id
+	} catch (error) {
+		console.error(error)
+		return null
+	}
 }
 
 export async function findById(id) {
-  try {
-    const [rows] = await DbMysql.query(
-      "SELECT id, name, email, created_at FROM app_users WHERE id = ?",
-      [id],
-    );
-    return _.head(rows) ?? null;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+	try {
+		const user = await AppUser.findOne({
+			where: { id },
+			attributes: ['id', 'name', 'email', 'created_at']
+		})
+		return _.defaultTo(user, null)
+	} catch (error) {
+		console.error(error)
+		return null
+	}
 }
 
-export default { findByEmail, createUser, findById };
+export default { findByEmail, createUser, findById }
