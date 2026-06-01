c2ab3250c1993ce561b9b9ec1a6f5f4ba501dbe8
Hovo Serobyan <129414253+Hovhannes09@users.noreply.github.com>
2026-06-02 00:14:56 +0400

    Added Front

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-import cookieParser from "cookie-parser";
-import "dotenv/config";
-import express from "express";
-import morgan from "morgan";
+import cookieParser from 'cookie-parser'
+import 'dotenv/config'
+import express from 'express'
+import morgan from 'morgan'
 
-import { migrate } from "./migrate.js";
-import router from "./routes/index.js";
+import { migrate } from './migrate.js'
+import router from './routes/index.js'
 
-const app = express();
+const app = express()
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(express.static('public'))
 
-await migrate();
+await migrate()
 
-app.use(router);
+app.use(router)
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
-});
+	console.log(`Server running on http://localhost:${process.env.PORT}`)
+})
